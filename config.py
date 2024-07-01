2f1790ddfd2a8cabbc898dd4eaaaa8d04aa03bbe
trrint <francisco.p.dm2003@gmail.com>
2024-07-01 14:38:00 -0300

    Atualização do arquivo config.

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///mydatabase.db'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'your_jwt_secret_key'
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
