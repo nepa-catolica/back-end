40f1cc994518261b877f660101f0da2efeab691c
Gustavo-Gadelha <gustavofg2112@gmail.com>
2025-03-17 14:56:53 -0300

    Tempo de expiração do token de acesso JWT aumentado para 2 horas

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from flask import Flask
 from dotenv import load_dotenv
 from src.utils.extensions import db, jwt, migrate, ma
@@ -12,6 +14,7 @@ def create_app():
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = os.getenv('JWT_SECRET_KEY')
+    app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
 
     db.init_app(app)
     migrate.init_app(app, db)
